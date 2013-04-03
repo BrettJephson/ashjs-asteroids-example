48466195e2169985597909fcf5dc1f687d685071
abiyasa <ilham.suhardi@gmail.com>
2013-04-03 16:42:42 +0200

    keypoll uses requirejs definition
    make it a singleton & also remove reference to
    global variable

--- src/asteroids-app.js
+++ src/asteroids-app.js
@@ -1,6 +1,10 @@
-define( 'asteroids-app', ["brejep/fillsnfixes", "game/asteroids"],
-    function( Fixes, Asteroids ) {
-        "use strict";
+require([
+    'brejep/fillsnfixes',
+    'brejep/keypoll',
+    'game/asteroids'
+], function(Fixes, KeyPoll, Asteroids) {
+        'use strict';
+
         function AsteroidsApp() {
             var CANVAS_WIDTH = 800,
                 CANVAS_HEIGHT = 600;
@@ -11,22 +15,28 @@ define( 'asteroids-app', ["brejep/fillsnfixes", "game/asteroids"],
                 Fixes.initialise();
 
                 var canvasElem = createCanvas();
-                document.getElementById( "game_wrapper" ).appendChild( canvasElem );
+                var gamewrapper = document.getElementById('game_wrapper');
+                gamewrapper.appendChild(canvasElem);
+
+                // init keyboard
+                KeyPoll.initialise(gamewrapper);
 
                 var asteroids = new Asteroids();
-                asteroids.initialise( canvasElem );
+                asteroids.initialise(canvasElem);
                 asteroids.start();
-            }
+            };
 
             function createCanvas() {
-                var canvasElem = document.createElement( "canvas" );
-                canvasElem.setAttribute( "id", "game_stage" );
-                canvasElem.setAttribute( "width", CANVAS_WIDTH );
-                canvasElem.setAttribute( "height", CANVAS_HEIGHT );
-                canvasElem.style.backgroundColor = "#000";
+                var canvasElem = document.createElement('canvas');
+                canvasElem.setAttribute('id', 'game_stage');
+                canvasElem.setAttribute('width', CANVAS_WIDTH);
+                canvasElem.setAttribute('height', CANVAS_HEIGHT);
+                canvasElem.style.backgroundColor = '#000';
                 return canvasElem;
             }
         }
-        return new AsteroidsApp();
+
+        // start!
+        new AsteroidsApp().initialise();
     }
 );
